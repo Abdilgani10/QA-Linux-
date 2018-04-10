c18e438cc75ca5303b496e526e23945d1245fb0b
Abdilgani <aabdilgani@gmail.com>
2018-04-10 17:30:29 +0100

    massage

---
+++ Advancedloop.sh
@@ -0,0 +1,13 @@
+
+for i in `seq 1 30`
+do
+    if [ `expr $i % 3` -eq 0 ] && [ `expr $i % 5` -eq 0 ]; then
+        echo "fizzbuzz"
+    elif [ `expr $i % 3` -eq 0 ]; then
+        echo "fizz"
+    elif [ `expr $i % 5` -eq 0 ]; then
+        echo "buzz"
+    else
+        echo $i
+    fi
+done
